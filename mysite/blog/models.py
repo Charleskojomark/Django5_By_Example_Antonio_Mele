fa8045e2dc0cb0cb4eb796ace2bb410610376f6e
Charleskojomark <markcharleskojo@gmail.com>
2025-01-29 20:24:33 +0100

    added custom object manager

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return(super().get_queryset().filter(status=Post.Status.PUBLISHED))
+
 class Post(models.Model):
     
     class Status(models.TextChoices):
@@ -16,6 +20,9 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
+    
+    objects = models.Manager()
+    published = PublishedManager()
     
     class Meta:
         ordering = ['-publish']
