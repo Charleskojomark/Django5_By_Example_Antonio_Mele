90c54f6eef64d2165e3e95e933e852abc85d8532
Charleskojomark <markcharleskojo@gmail.com>
2025-01-30 11:47:47 +0100

    added pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from blog.models import Post
-from django.http import Http404
+# from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Paginator with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 # def post_detail(request, id):
@@ -15,6 +27,11 @@ def post_list(request):
 #         raise Http404('No Post Found')
 #     return render('blog/post/detail.html', {'post': post})
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+# def post_detail(request, id):
+#     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+#     return render(request, 'blog/post/detail.html', {'post':post})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post':post})
+
